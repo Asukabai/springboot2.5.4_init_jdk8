24f898cc358be1f1cf2ef2d9cc1072084c6a298b
白鲤鱼 <3293775641@qq.com>
2024-09-11 11:51:31 +0800

    修改文件解析的方式，并增加一定的方法注释

--- src/main/java/com/ss/system/util/BilibiliUtils.java
+++ src/main/java/com/ss/system/util/BilibiliUtils.java
@@ -10,52 +10,73 @@ import java.util.Scanner;
 public class BilibiliUtils {
 
     public static void main(String[] args) {
+
+        // 创建一个 File 对象，指向 "D:\\UP" 目录，这是程序开始搜索的根目录。
         File rootDir = new File("D:\\UP");
-        renameFiles(rootDir);
+        // 调用 processDirectory 方法来处理该目录及其子目录。
+        processDirectory(rootDir);
     }
 
-    private static void renameFiles(File dir) {
+    // 声明一个私有的静态方法 processDirectory，接受一个 File 对象作为参数。
+    private static void processDirectory(File dir) {
+        // if (dir.isDirectory()) 确保传入的 File 对象是一个目录。
         if (dir.isDirectory()) {
+            // dir.listFiles() 返回一个 File 数组，包含目录中的所有文件和子目录。
             File[] files = dir.listFiles();
             if (files != null) {
                 for (File file : files) {
+                    // 递归处理子目录：如果是子目录，递归调用 processDirectory 方法来进一步处理。
                     if (file.isDirectory()) {
-                        renameFiles(file); // Recursively search within subdirectories
-                    } else {
-                        if (file.getName().equalsIgnoreCase("entry.json")) {
-                            processJsonFile(file.getParentFile());
-                        }
+                        processDirectory(file);
+                        // 如果找到名为 entry.json 的文件，调用 processJsonFile 方法进行处理。
+                    } else if (file.getName().equalsIgnoreCase("entry.json")) {
+                        processJsonFile(dir);
                     }
                 }
             }
         }
     }
 
-    private static void processJsonFile(File parentDir) {
-        File jsonFile = new File(parentDir, "entry.json");
+
+    // 声明一个私有的静态方法 processJsonFile，接受一个 File 对象作为参数，这个 File 对象应指向包含 entry.json 文件的目录。
+    private static void processJsonFile(File jsonDir) {
+        // new File(jsonDir, "entry.json") 创建一个指向 entry.json 文件的 File 对象。
+        File jsonFile = new File(jsonDir, "entry.json");
         if (jsonFile.exists()) {
+            // 使用 FileReader 和 Scanner 读取文件内容。
             try (FileReader reader = new FileReader(jsonFile);
                  Scanner scanner = new Scanner(reader)) {
+                // scanner.useDelimiter("\\A").next() 读取整个文件内容为一个字符串。
                 String jsonString = scanner.useDelimiter("\\A").next();
+                // 使用 JsonParser 解析 JSON 字符串，并转换为 JsonObject。
                 JsonObject jsonObject = JsonParser.parseString(jsonString).getAsJsonObject();
-
                 String title = jsonObject.get("title").getAsString();
                 String ownerName = jsonObject.get("owner_name").getAsString();
                 String newBaseName = title + " - " + ownerName;
-
                 System.out.println("New base name: " + newBaseName);
-
-                File videoFile = new File(parentDir, "video.m4s");
-                File audioFile = new File(parentDir, "audio.m4s");
-
-                renameFileIfNeeded(videoFile, newBaseName + ".mp4");
-                renameFileIfNeeded(audioFile, newBaseName + ".mp3");
-
+                // 调用 searchAndRenameFiles 方法，在当前目录中查找 video.m4s 和 audio.m4s 文件并进行重命名。
+                searchAndRenameFiles(jsonDir, newBaseName);
             } catch (IOException e) {
                 System.out.println("Failed to read or process JSON file: " + e.getMessage());
             }
         } else {
-            System.out.println("entry.json not found in directory: " + parentDir.getAbsolutePath());
+            System.out.println("entry.json not found in directory: " + jsonDir.getAbsolutePath());
+        }
+    }
+
+    private static void searchAndRenameFiles(File startDir, String newBaseName) {
+        File[] files = startDir.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    // Recursively search in subdirectories
+                    searchAndRenameFiles(file, newBaseName);
+                } else if (file.getName().equalsIgnoreCase("video.m4s")) {
+                    renameFileIfNeeded(file, newBaseName + ".mp4");
+                } else if (file.getName().equalsIgnoreCase("audio.m4s")) {
+                    renameFileIfNeeded(file, newBaseName + ".mp3");
+                }
+            }
         }
     }
 
